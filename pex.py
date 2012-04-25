2e8aa7f5b6a9eb10e146a6fec201cecd410a4da5
Roberto Bonvallet <rbonvall@gmail.com>
2012-04-24 23:08:40 -0300

    We'll need to parse command line options

---
+++ pex.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import optparse
+
+
+def main():
+    parser = optparse.OptionParser()
+    (options, args) = parser.parse_args()
+
+
+if __name__ == '__main__':
+    main()
