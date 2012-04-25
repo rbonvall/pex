6f4d46dabdcd05cafbd54a0b65f57712e2536353
Roberto Bonvallet <rbonvall@gmail.com>
2012-04-24 23:42:12 -0300

    Separate the command from its args

--- pex.py
+++ pex.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python
 
 import optparse
+import sys
 
 
 def main():
     parser = optparse.OptionParser()
     (options, args) = parser.parse_args()
+
+    if not args:
+        parser.print_help()
+        sys.exit(-1)
+
+    command, rest = args[0], args[1:]
 
 
 if __name__ == '__main__':
