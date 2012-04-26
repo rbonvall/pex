c97d8ef13d53b70003323a942674a49b6b23426c
Roberto Bonvallet <rbonvall@gmail.com>
2012-04-25 23:06:58 -0300

    The set_defaults(func=) trick
    As described in
    http://docs.python.org/library/argparse.html#sub-commands

--- pex.py
+++ pex.py
@@ -3,24 +3,38 @@
 import argparse
 import sys
 
+def pex_new(args):
+    print 'New'
+    print args
+
+def pex_install(args):
+    print 'Install'
+    print args
+
+def pex_help(args):
+    print 'Help'
+    print args
 
 def main():
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers()
 
     new_parser = subparsers.add_parser('new')
+    new_parser.set_defaults(func=pex_new)
     new_parser.add_argument('template_name')
     new_parser.add_argument('directory')
 
     install_parser = subparsers.add_parser('install')
+    install_parser.set_defaults(func=pex_install)
     install_parser.add_argument('location')
     install_parser.add_argument('template_name')
 
     help_parser = subparsers.add_parser('help')
+    help_parser.set_defaults(func=pex_help)
     help_parser.add_argument('command')
 
-    ns = parser.parse_args()
-    print ns
+    args = parser.parse_args()
+    args.func(args)
 
 
 if __name__ == '__main__':
